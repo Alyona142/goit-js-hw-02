d240effbc5b428de6f5f9d809d6da7664b427fa4
Alyona <olenapav97@gmail.com>
2024-08-28 22:24:31 +0300

    аа

---
+++ js/task-3.js
@@ -0,0 +1,6 @@
+function checkForSpam(message) {
+    const lowerCaseMessage = message.toLowerCase();
+    if (lowerCaseMessage.includes(`spam`) || lowerCaseMessage.includes(`sale`)) {
+        return true; 
+  }
+}
